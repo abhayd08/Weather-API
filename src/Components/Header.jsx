645d91dc7845cecdba3cf94e1a250463664ba7ae
Abhay <abhaydt.08@gmail.com>
2023-07-27 16:12:09 +0530

    DEBUUGED THE HEADER AND FOOTER FILES

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,15 +2,22 @@ import Box from '@mui/material/Box';
 import "./Header.css"
 
 function Header({weatherData}) {
-
+  if(weatherData){
+    document.getElementById("header").style.height = "auto"
+  }
+  else{
+    if(document.getElementById("header")){
+      document.getElementById("header").style.height = "3rem" 
+    }
+  }
   return (
     <Box id = "header">
-            {weatherData? <Box>
+            {weatherData? <Box className = "my-3">
                             <img src = {weatherData.current.condition.icon}
                               style = {{height: "40px", width: "40px"}}
                               alt = "weather-logo"
                             />
-                            <strong className = "mx-2 text-body text-decoration-none">{`${weatherData.location.name} |`}</strong>
+                            <strong className = "mx-1 text-body text-decoration-none">{`${weatherData.location.name} |`}</strong>
                             <strong className = "text-body-secondary text-decoration-none">{weatherData.current["temp_c"]}°C</strong>
                           </Box>: ""
             }
